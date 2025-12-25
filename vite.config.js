7c7e8e180f5f00ce079b00e7c4e5b876616e0678
Xingyan <nxy1027@163.com>
2025-12-25 21:15:00 +0800

    add Github Pages actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const VITE_BASE_URL = process.env.VITE_BASE_URL || ""
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: VITE_BASE_URL,
   plugins: [react()],
 })
